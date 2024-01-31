cf9ecd40701b75aadd834e794e30b466d418e85e
Bayad <bayad999.bd@gmail.com>
2024-01-31 09:25:34 +0300

    description

---
+++ android/app/src/main/kotlin/com/example/iam_happy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iam_happy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          "iam happy",
+          style: TextStyle(color: Colors.white),
+        )),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    body: Center(child: Image(image: AssetImage('images/happy.jpg'))),
+      
+    ));
+  }
+}
